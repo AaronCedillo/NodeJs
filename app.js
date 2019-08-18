cea36026e439c83f14e41d10663245aad58d9e1f
AaronCedillo <adrian_sonny55@hotmail.com>
2019-08-18 18:38:20 -0500

    Tablas, se crea un archivo con la tabla de multiplicar

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-let base = 2;
+//Requierds
+
+const fs = require('fs');
+
+let base = 5;
+let data = '';
 
 for(let i = 0; i < 11; ++i) {
   let newBase = base * i;
-  console.log(` ${base} * ${i} = ${newBase}`);
+  data +=` ${base} * ${i} = ${newBase}\n`;
 }
+
+fs.writeFile(`tabla del ${base} .txt`, data, (err) => {
+  if(err) throw err;
+
+  console.log('El archivo tabla ha sido creado');
+})
